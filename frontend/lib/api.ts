2b9991243e724bf5974ef3fa9a91633bbb6e6534
sarathj7 <sarathjece@gmail.com>
2026-02-07 00:18:20 +0100

    Update api.ts
    for deploy

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8000";
+const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL;
 
 export const api = axios.create({
   baseURL: apiBaseUrl,
